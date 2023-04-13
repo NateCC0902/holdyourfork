59c012cfe88daa3efd7312f4a7003584a8687950
NateCC0902 <r7895566@gmail.com>
2023-04-13 19:59:57 +0800

    a
    z

---
+++ READEME.md
@@ -0,0 +1,12 @@
+you need puppeteer to run the script.
+'''
+pip puppeteer
+'''
+
+and config you password and you need 
+
+let username = '';
+let password = '';
+let course = 'AAAAA';
+
+and in main.js you also need set DUO passcode (dont use push). 
---
+++ main.js
@@ -0,0 +1,62 @@
+const puppeteer = require('puppeteer');
+import userData from './password.js';
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: [
+            '--disable-web-security',
+            '--disable-features=IsolateOrigins,site-per-process'
+        ]
+    });
+    //init
+    const page = await browser.newPage();
+    await page.goto('https://wrem.sis.yorku.ca/Apps/WebObjects/REM.woa/wa/DirectAction/rem');
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    //login
+    await page.type('#mli', userData.username);
+    await page.type('#password', userData.password)
+    const btn = '.btn.btn-lg.btn-primary';
+    await page.waitForSelector(btn);
+    await page.click(btn);
+
+    //duo
+    await page.waitForTimeout(10000);
+    const frames = page.frames();
+    const duoF = frames[1];
+    if (duoF) {
+        await duoF.$eval('#passcode', el => el.click());
+        await duoF.$eval('.passcode-input', el => el.value = '');
+        await duoF.$eval('#passcode', el => el.click());
+    }
+    //select term
+    await page.waitForTimeout(10000);
+    await page.select('select[name="5.5.1.27.1.11.0"]', '0'); //summer
+    await page.waitForSelector('input[type=submit]');
+    await page.click('input[type=submit]');
+
+    while (true) {
+        //loop slect course
+        await page.waitForTimeout(5000);
+        await page.waitForSelector('input[name="5.1.27.1.23"]');
+        await page.click('input[name="5.1.27.1.23"]');
+
+        await page.waitForTimeout(5000);
+        await page.type('input[name="5.1.27.7.7"]', userData.course);
+        await page.click('input[name="5.1.27.7.9"]');
+
+        await page.waitForSelector('input[name="5.1.27.11.11"]');
+        await page.click('input[name="5.1.27.11.11"]');
+
+        await page.waitForTimeout(5000);
+        await page.waitForSelector('input[name="5.1.27.27.11"]');
+        await page.click('input[name="5.1.27.27.11"]');
+
+        // wait 15 minutes 
+        await delay(900000);
+
+    }
+
+})();
+
